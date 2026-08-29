c2f9120ce0cbfda7cb3aa552481cc1da9a4d0fbe
Bastillze <36552461+Bastillze@users.noreply.github.com>
2026-08-29 19:02:57 -0400

    Second Commit_Building Functions

---
+++ Linkedlist.js
@@ -0,0 +1,18 @@
+class linkedList{
+    new list() {
+        this.head = null;
+    }
+}
+
+class Node{
+    new Node(value,next = null){
+    this.value = value;
+    this.next = next;
+    }
+}
+
+let projectLinkedList = new linkedList();
+
+function append(value) {
+    head = value.append;
+}

9ffc2313ef739af96a06acd0bc90f92e66b8e73a
Bastillze <36552461+Bastillze@users.noreply.github.com>
2026-08-30 09:18:51 -0400

    Third Commit
    Updated code

--- Linkedlist.js
+++ Linkedlist.js
@@ -16,3 +16,58 @@ let projectLinkedList = new linkedList();
 function append(value) {
     head = value.append;
 }
+
+function prepend(value){
+    head = addFirst(value);
+}
+
+function size(){
+    projectLinkedList.size();
+    return
+}
+
+function head(){
+    if(head==null){
+        return undefined;
+    }
+    else{
+        return this.value;
+    }
+}
+
+function tail(){
+    tail = addLast(value);
+}
+
+function index(){
+    if(current==value){
+        return current.next;
+    }
+    else{
+        return undefined;
+    }
+}
+
+function pop(){
+    head.pop();
+    return value;
+}
+
+function contains(value){
+    if(this.value==value){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function findIndex(value){
+    if(this.value==value){
+        return this.value;
+    }
+}
+
+function toString(){
+    projectLinkedList.toString();
+}
